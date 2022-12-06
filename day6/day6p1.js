89b309045a0f73494c64cce5af9e2773e4e30f24
Philipp Schuck <philipp.schuck@gmx.de>
2022-12-06 08:10:35 +0100

    Day 6

---
+++ day6/day6p1.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const lines = fs
+	.readFileSync("data.txt", {
+		encoding: "utf-8"
+	})
+
+for (let i = 0; i < lines.length; i++) {
+	var marker = lines.substring(i, i + 4)
+	var bMarker = checkString(marker)
+	if (bMarker) {
+		console.log(i + 4)
+		break;
+	}
+}
+
+
+function checkString(str) {
+	for (let i = 0; i < str.length; i++)
+		for (let j = i + 1; j < str.length; j++)
+			if (str[i] == str[j])
+				return false;
+	return true;
+}
